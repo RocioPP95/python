1b9e8092738e7724e6afef0369d1fa3319e0105e
ft <tr>
2025-10-22 18:20:54 +0200

    .

--- Practica4.py
+++ Practica4.py
@@ -1,4 +1,6 @@
-print(CONVERTIDOR DE GRADOS CELSIUS A GRADOS FAHRENHEIT)
+print("CONVERTIDOR DE GRADOS CELSIUS A GRADOS FAHRENHEIT")
+
 celsius = float(input("Escriba una temperatura en grados Celsius: "))
 fahrenheit = 1.8 * celsius + 32
+
 print(celsius, "ºC son", fahrenheit, "ºF")
