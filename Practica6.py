13e1305e8ea87d2eecfea6c60622b628fe6f2c22
ft <tr>
2025-10-22 17:57:50 +0200

    .

--- Practica6.py
+++ Practica6.py
@@ -4,6 +4,6 @@ print("CONVERTIDOR DE SEGUNDOS A MINUTOS")
 segundos = int(input("Escriba una cantidad de segundos: "))
 
 minutos = round(segundos / 60, 2)
-resto_segundos = round(segundos % 60, 2)
+restoSegundos = round(segundos % 60, 2)
 
 print(segundos, "segundos son", minutos, "minutos y", restoSegundos, "segundos")
--- Practica7.py
+++ Practica7.py
@@ -4,6 +4,6 @@ segundos = int(input("Escriba una cantidad de segundos: "))
 
 horas = round(segundos / 3600, 2)
 minutos = round((segundos % 3600) / 60, 2)
-resto_segundos = round(segundos % 60, 2)
+restoSegundos = round(segundos % 60, 2)
 
 print(segundos, "segundos son", horas, "horas,", minutos, "minutos y", restoSegundos, "segundos")
